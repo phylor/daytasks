1e519d492e061ba60f11b51ae586a6afee4e507e
Serge Hänni <serge.haenni@simplificator.com>
2017-09-06 11:28:07 +0200

    added tasks to be added to the current day

--- Daytask.elm
+++ Daytask.elm
@@ -1,5 +1,7 @@
-import Html exposing (program, div, text, button)
-import Html.Events exposing (onClick)
+import Html exposing (program, div, text, button, input, Attribute)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onClick, onInput, on, keyCode)
+import Json.Decode
 import Date exposing (Date)
 import Task
 import HeroList exposing (HeroList)
@@ -7,7 +9,17 @@ import Date.Extra.Duration
 
 type Model
   = Loading
-  | LoadedModel DayTaskModel
+  | LoadedModel (WebModel TemporaryModel DayTaskModel)
+
+type alias WebModel a b =
+  { temporary : a
+  , dirty : b
+  , persisted : b
+  }
+
+type alias TemporaryModel =
+  { newTask : String
+  }
 
 type alias DayTaskModel =
   HeroList DayTask
@@ -34,6 +46,8 @@ type Message
   = CreateTodayDayTask Date
   | PreviousDay
   | NextDay
+  | InputNewTask String
+  | KeyDown Int
 
 init =
   ( Loading, createTodayDayTask )
@@ -46,25 +60,47 @@ update message model =
     Loading ->
       case message of
         CreateTodayDayTask date ->
-          ( LoadedModel <| HeroList.singleton (DayTask date []), Cmd.none )
-        PreviousDay ->
+          let
+            newHeroList = HeroList.singleton (DayTask date [])
+          in
+            ( LoadedModel <| WebModel (TemporaryModel "") newHeroList newHeroList, Cmd.none )
+        _ ->
           ( model, Cmd.none )
-        NextDay ->
-          ( model, Cmd.none )
-    LoadedModel list ->
+    LoadedModel model ->
       case message of
         CreateTodayDayTask _ ->
-          ( model, Cmd.none )
+          ( LoadedModel model, Cmd.none )
         PreviousDay ->
           let
-            previousDay = Date.Extra.Duration.add Date.Extra.Duration.Day -1 list.current.day
+            previousDay = Date.Extra.Duration.add Date.Extra.Duration.Day -1 model.dirty.current.day
           in
-            ( LoadedModel <| HeroList.previousOrCreate list (DayTask previousDay []), Cmd.none )
+            ( LoadedModel <| WebModel model.temporary (HeroList.previousOrCreate model.dirty (DayTask previousDay [])) model.persisted, Cmd.none )
         NextDay ->
           let
-            nextDay = Date.Extra.Duration.add Date.Extra.Duration.Day 1 list.current.day
+            nextDay = Date.Extra.Duration.add Date.Extra.Duration.Day 1 model.dirty.current.day
           in
-            ( LoadedModel <| HeroList.nextOrCreate list (DayTask nextDay []), Cmd.none )
+            ( LoadedModel <| WebModel model.temporary (HeroList.nextOrCreate model.dirty (DayTask nextDay [])) model.persisted, Cmd.none )
+        InputNewTask text ->
+          let
+            temporaryModel = model.temporary
+            newTemporaryModel = { temporaryModel | newTask = text }
+          in
+            ( LoadedModel { model | temporary = newTemporaryModel }, Cmd.none )
+        KeyDown 13 ->
+          let
+            newTaskTitle = model.temporary.newTask
+            temporaryModel = model.temporary
+            newTemporaryModel = { temporaryModel | newTask = "" }
+            newTask = Task newTaskTitle model.dirty.current.day model.dirty.current.day
+            dayModel = model.dirty.current
+            newDayModel = { dayModel | tasks = newTask :: dayModel.tasks }
+            dirty = model.dirty
+            newDirty = { dirty | current = newDayModel }
+          in
+          -- save task: add DayTask for the current day
+            ( LoadedModel { model | temporary = newTemporaryModel, dirty = newDirty }, Cmd.none )
+        KeyDown _ ->
+          ( LoadedModel model, Cmd.none )
 
 view model =
   case model of
@@ -74,6 +110,16 @@ view model =
     LoadedModel model ->
       div []
         [ button [ onClick PreviousDay ] [ text "<" ]
-        , div [] [ text <| toString model.current.day ]
+        , div [] [ text <| toString model.dirty.current.day ]
         , button [ onClick NextDay ] [ text ">" ]
+        , div []
+            (List.map renderTask model.dirty.current.tasks)
+        , input [ onInput InputNewTask, onKeyDown KeyDown, value model.temporary.newTask ] []
         ]
+
+onKeyDown : (Int -> Message) -> Attribute Message
+onKeyDown tagger =
+  on "keydown" (Json.Decode.map tagger keyCode)
+
+renderTask task =
+  div [] [ text task.title ]
